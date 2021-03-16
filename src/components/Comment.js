70005bc6ac21a5ac4d4a320bade05e5b20cde4d9
Dayne Dewar <daynedewar@yahoo.com>
2021-03-16 17:12:46 -0400

    likes/dislikes

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,10 +1,22 @@
-
-
+import React, { useState } from "react";
 function Comment({name, comment}) {
+    const [likes, setLikes] = useState(0)
+    const [dislikes, setDislikes] = useState(0)
+
+    function increaseLikes() {
+        return setLikes(likes + 1)
+    }
+    function increaseDislikes() {
+        return setDislikes(dislikes + 1)
+    }
     return (
         <div>
-            <p>{name}</p>
+            <h4>{name}</h4>
             <p>{comment}</p>
+            <button onClick={increaseLikes}>{likes}👍🏽</button>
+            <button onClick={increaseDislikes}>{dislikes}👍🏽</button>
+            <button onClick>❌</button>
+
         </div>
 )}
 
